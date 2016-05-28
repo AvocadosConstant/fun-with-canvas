bc3eaae34b336ae17cea7184a42bc01f08e5a8fa
Tim <thung1@binghamton.edu>
2016-05-28 04:23:13 -0400

    Fix looping and add styling

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-function outlineShape(pts) {
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
+function outlineShape(ctx, pts) {
   ctx.beginPath();
   ctx.moveTo(pts[0].x ,pts[0].y);
-  for(i = 1; i < pts.length; i++) {
-    ctx.lineTo(pts[i].x, pts[i].y);
+  for(i = 0; i < pts.length; i++) {
+    ctx.lineTo(pts[(i+1)%pts.length].x, pts[(i+1)%pts.length].y);
   }
-  ctx.closePath();
   ctx.stroke();
 }
 
@@ -15,104 +12,55 @@ function nestShape(pts, factor) {
   for(i = 0; i < pts.length; i++) {
     a = pts[i];
     b = pts[(i+1)%pts.length];
-
     dx = b.x - a.x;
     dy = b.y - a.y;
     dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-    
     xOffset = factor*dx/dist;
     yOffset = factor*dy/dist;
-
     nestPts.push({x: a.x + xOffset, y: a.y + yOffset});
   }
   return nestPts;
 }
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var canvasSize = {width: 400, height: 400};
-
-ctx.fillStyle = "#EEE";
-ctx.fillRect(0, 0, canvasSize.width, canvasSize.height);
-
-var pts = []; 
-for(i = 0; i < 16; i++) {
-  pts.push({x: Math.random()*canvasSize.width, y: Math.random()*canvasSize.height});
-  console.log(pts[i].x + ", " + pts[i].y);
+function generatePts(num, minX, maxX, minY, maxY) {
+  var pts = []; 
+  for(i = 0; i < num; i++) {
+    pts.push({
+      x: Math.random() * (maxX - minX) + minX, 
+      y: Math.random() * (maxY - minY) + minY 
+    });
+  }
+  return pts;
 }
-outlineShape(pts);
 
-// Using a loop breaks this. Probably some limitation from canvas drawing async stuff. 
-// Need to do more research.
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
-pts = nestShape(pts, 4);
-outlineShape(pts);
+function drawNestedConstruction(pts) {
+  for(iter = 0; iter < 24; iter++) {
+    outlineShape(ctx, pts);
+    pts = nestShape(pts, 12);
+  }
+}
+
+
+
+
+var CANVAS = {
+  id: "canvas",
+  width: 800, 
+  height: 600,
+  border: 10
+};
+
+var canvas = document.getElementById(CANVAS.id);
+var ctx = canvas.getContext("2d");
+
+//  draw canvas
+ctx.fillStyle = "#fafcdc";
+ctx.fillRect(0, 0, CANVAS.width, CANVAS.height);
+
+drawNestedConstruction(
+    generatePts(
+      32, 
+      CANVAS.border, CANVAS.width - CANVAS.border, 
+      CANVAS.border, CANVAS.height - CANVAS.border
+    )
+);
